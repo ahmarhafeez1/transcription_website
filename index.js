67c736dd4f6554a27b594e8742c9f0f2a5fd2ad2
ehmargondal@gmail.com <ehmargondal@gmail.com>
2023-06-20 00:20:55 +0500

    Added Cors and Yarn

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express =require('express')
 const connectDB = require('./config/connect.js');
 const userRoutes = require('./routes/UserRoutes')
 const transcriptionRouter = require('./routes/transcriptionRoutes.js')
+const cors = require("cors");
 require('dotenv').config();
 
 
@@ -10,6 +11,7 @@ require('dotenv').config();
 const PORT = process.env.PORT
 connectDB()
 const app = express()
+app.use(cors());
 app.use(express.json())
 
 app.use('/api/users', userRoutes);
